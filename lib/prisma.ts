ae1871996d3db0c59129f12617c32a6cfbcfbf92
jabulani Mokoena <mokoenajabulani730@gmail.com>
2026-03-19 01:36:28 +0200

    deployment

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,16 +1,20 @@
 import { PrismaClient } from "../app/generated/prisma/client"
+import { withAccelerate } from "@prisma/extension-accelerate"
 import { PrismaPg } from "@prisma/adapter-pg"
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
-}
+const globalForPrisma = globalThis as unknown as { prisma: any }
 
 function createPrismaClient() {
-  const connectionString = process.env.DIRECT_URL || 
-    process.env.DATABASE_URL || 
-    "postgres://postgres:postgres@localhost:51214/template1?sslmode=disable"
+  if (process.env.NODE_ENV === "production") {
+    const client = new PrismaClient({
+      accelerateUrl: process.env.DATABASE_URL!,
+    })
+    return client.$extends(withAccelerate()) as any
+  }
 
-  const adapter = new PrismaPg({ connectionString })
+  const adapter = new PrismaPg({
+    connectionString: "postgres://postgres:postgres@localhost:51214/template1?sslmode=disable"
+  })
   return new PrismaClient({ adapter })
 }
 
